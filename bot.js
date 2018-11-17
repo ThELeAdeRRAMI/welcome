bb79a4c257f8c6bac2bafcdb64f3ea44ab03d11a
ThELeAdeRRAMI <44674539+ThELeAdeRRAMI@users.noreply.github.com>
2018-11-17 18:22:14 +0200

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,7 @@
+client.on('guildMemberAdd', member => {
+const mohamed= member.guild.channels.get("512734412501155840");
+if(!mohamed) return;
+if(mohamed) {
+setTimeout(() => mohamed.send(`*Welcome To* ***Fèarless*** *Server*..`), 4000)        
+}
+});
