a658344769e49df2366fe2c18b0a348a8c33b688
ThELeAdeRRAMI <44674539+ThELeAdeRRAMI@users.noreply.github.com>
2018-11-17 18:23:48 +0200

    Delete bot.js

--- bot.js
+++
@@ -1,7 +0,0 @@
-client.on('guildMemberAdd', member => {
-const mohamed= member.guild.channels.get("512734412501155840");
-if(!mohamed) return;
-if(mohamed) {
-setTimeout(() => mohamed.send(`*Welcome To* ***Fèarless*** *Server*..`), 4000)        
-}
-});
